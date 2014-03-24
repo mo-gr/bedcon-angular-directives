ef973416c4f341ecbd068d8b1b3f509c1c63e78d
Moritz Grauel <mo@notadomain.com>
2014-03-24 21:52:08 +0100

    kitchensink

---
+++ examples/drinks/js/app.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+    var app = angular.module('DrinksMachine', []);
+
+    app.controller('SelectionController', function ($scope) {
+        var model = {
+            selectedDrink: ''
+        };
+
+        $scope.selectDrink = function (selection) {
+            model.selectedDrink = selection;
+        };
+
+        $scope.model = model;
+    });
+
+    app.directive('moNoise', function () {
+        return {
+            link: function (scope, element, attrs) {
+                var sound = new Howl({
+                    urls: [attrs.moNoise]
+                });
+                element.on('click', function () {sound.play();});
+            }
+        };
+    });
+
+    app.directive('drinkSelect', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'drink-select.html',
+            replace: true,
+            scope: {
+                drink: '@',
+                selection: '='
+            }
+        }
+    });
+}());
