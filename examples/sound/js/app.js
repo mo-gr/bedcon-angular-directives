ca7b7273f6aa60e14be175ca2bbd3b258741eb55
Moritz Grauel <mo@notadomain.com>
2014-03-29 23:21:31 +0100

    tests

--- examples/sound/js/app.js
+++ examples/sound/js/app.js
@@ -2,10 +2,10 @@
     'use strict';
     var app = angular.module('Example', []);
 
-    app.directive('noise', function() {
+    app.directive('noise', function($window) {
         return {
             link: function (scope, element, attrs) {
-                var sound = new Howl({
+                var sound = new $window.Howl({
                     urls: [attrs.noise],
                     onplay: function () {
                         scope.$apply(function () {
